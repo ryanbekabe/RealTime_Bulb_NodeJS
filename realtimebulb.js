9e52fd8776606d383bf3c7c1505a7520c726e89b
ryanbekabe <bekabeipa@gmail.com>
2020-09-21 11:05:01 +0700

    Update realtimebulb.js

--- realtimebulb.js
+++ realtimebulb.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res,next) {
 	res.sendFile(__dirname + '/realtimebulb.html');
